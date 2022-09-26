6081c51df77885a32dc4a9dfba8acb53a46b4eec
Fabrizio <fabrizio2k2@gmail.com>
2022-09-26 14:06:18 -0400

    Commit: Finished Week2

--- app/src/main/java/algonquin/cst2335/rugg0011/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/rugg0011/ui/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import algonquin.cst2335.rugg0011.data.MainViewModel;
 import algonquin.cst2335.rugg0011.databinding.ActivityMainBinding;
@@ -16,10 +19,13 @@ import algonquin.cst2335.rugg0011.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     private MainViewModel model;
     private ActivityMainBinding variableBinding;
+    private String compoundButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 
         model = new ViewModelProvider(this).get(MainViewModel.class);
 
@@ -30,8 +36,42 @@ public class MainActivity extends AppCompatActivity {
             model.editString.postValue(variableBinding.myEditText.getText().toString());
         });
 
+        variableBinding.checkBox.setOnCheckedChangeListener((btn, isSelected) -> {
+            model.isSelected.postValue(isSelected);
+            compoundButton = "CheckBox";
+        });
+
+        variableBinding.radioButton.setOnCheckedChangeListener((btn, isSelected) -> {
+            model.isSelected.postValue(isSelected);
+            compoundButton = "Radio Button";
+        });
+
+        variableBinding.switch1.setOnCheckedChangeListener((btn, isSelected) -> {
+            model.isSelected.postValue(isSelected);
+            compoundButton = "Switch";
+        });
+
+        variableBinding.myImageButton.setOnClickListener(click -> {
+            Context context = getApplicationContext();
+            CharSequence text = "Width= " + variableBinding.myImageView.getWidth() + "\nHeight= " + variableBinding.myImageView.getHeight();
+            int duration = Toast.LENGTH_SHORT;
+            Toast.makeText(context, text, duration).show();
+        });
+
         model.editString.observe(this, s -> {
             variableBinding.myText.setText("Your edit text has: \n" + s);
+        });
+
+        model.isSelected.observe(this, selected ->{
+            variableBinding.checkBox.setChecked(selected);
+            variableBinding.radioButton.setChecked(selected);
+            variableBinding.switch1.setChecked(selected);
+
+
+            Context context = getApplicationContext();
+            CharSequence text = "You've clicked on the " + compoundButton + " and it's now: " + selected;
+            int duration = Toast.LENGTH_SHORT;
+            Toast.makeText(context, text, duration).show();
         });
     }
 }
