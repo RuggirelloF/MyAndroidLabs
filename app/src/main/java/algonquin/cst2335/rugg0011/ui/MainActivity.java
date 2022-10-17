2375d9eaa5afa395d6e93a05c14aa78dfc46e2a0
Fabrizio <fabrizio2k2@gmail.com>
2022-10-17 04:46:47 -0400

    Finished Phone Version of Lab 3

--- app/src/main/java/algonquin/cst2335/rugg0011/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/rugg0011/ui/MainActivity.java
@@ -1,78 +1,40 @@
 package algonquin.cst2335.rugg0011.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
-import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.animation.*;
+import android.widget.*;
 
-import algonquin.cst2335.rugg0011.data.MainViewModel;
-import algonquin.cst2335.rugg0011.databinding.ActivityMainBinding;
+import algonquin.cst2335.rugg0011.R;
+
 
 public class MainActivity extends AppCompatActivity {
-    private MainViewModel model;
-    private ActivityMainBinding variableBinding;
-    private String compoundButton;
+
+    ImageView flagView;
+    Switch flagSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        flagView = findViewById(R.id.flagView);
+        flagSwitch = findViewById(R.id.flagSwitch);
 
+        flagSwitch.setOnCheckedChangeListener((btn, isChecked) -> {
+            if (isChecked) {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(1000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
 
-        model = new ViewModelProvider(this).get(MainViewModel.class);
-
-        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(variableBinding.getRoot());
-
-        variableBinding.myButton.setOnClickListener(click ->{
-            model.editString.postValue(variableBinding.myEditText.getText().toString());
+                flagView.startAnimation(rotate);
+            } else {
+                flagView.clearAnimation();
+            }
         });
 
-        variableBinding.checkBox.setOnCheckedChangeListener((btn, isSelected) -> {
-            model.isSelected.postValue(isSelected);
-            compoundButton = "CheckBox";
-        });
 
-        variableBinding.radioButton.setOnCheckedChangeListener((btn, isSelected) -> {
-            model.isSelected.postValue(isSelected);
-            compoundButton = "Radio Button";
-        });
-
-        variableBinding.switch1.setOnCheckedChangeListener((btn, isSelected) -> {
-            model.isSelected.postValue(isSelected);
-            compoundButton = "Switch";
-        });
-
-        variableBinding.myImageButton.setOnClickListener(click -> {
-            Context context = getApplicationContext();
-            CharSequence text = "Width= " + variableBinding.myImageView.getWidth() + "\nHeight= " + variableBinding.myImageView.getHeight();
-            int duration = Toast.LENGTH_SHORT;
-            Toast.makeText(context, text, duration).show();
-        });
-
-        model.editString.observe(this, s -> {
-            variableBinding.myText.setText("Your edit text has: \n" + s);
-        });
-
-        model.isSelected.observe(this, selected ->{
-            variableBinding.checkBox.setChecked(selected);
-            variableBinding.radioButton.setChecked(selected);
-            variableBinding.switch1.setChecked(selected);
-
-
-            Context context = getApplicationContext();
-            CharSequence text = "You've clicked on the " + compoundButton + " and it's now: " + selected;
-            int duration = Toast.LENGTH_SHORT;
-            Toast.makeText(context, text, duration).show();
-        });
     }
 }
 
