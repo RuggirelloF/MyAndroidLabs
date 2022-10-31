b1ccb6cddbab86ef294e446cec98c26d6676962b
Fabrizio <fabrizio2k2@gmail.com>
2022-10-31 14:24:02 -0400

    Finished Week4

--- app/src/main/java/algonquin/cst2335/rugg0011/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/rugg0011/MainActivity.java
@@ -1,14 +1,16 @@
 package algonquin.cst2335.rugg0011;
 
-import android.content.Intent;
+import android.content.*;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 import algonquin.cst2335.rugg0011.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding variableBinding;
+    SharedPreferences prefs;
+    private static final String SHARED_PREF_NAME = "MyData";
+    private static final String KEY_LOGIN = "email";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +20,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(variableBinding.getRoot());
 
         variableBinding.loginButton.setOnClickListener(clk -> {
-            Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
-            nextPage.putExtra("EmailAddress", variableBinding.editEmail.getText().toString());
-            startActivity(nextPage);
+            secondActivity();
         });
+
+        prefs = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+
+        String email = prefs.getString(KEY_LOGIN, null);
+
+        variableBinding.editEmail.setText(email);
+
+    }
+
+    public void secondActivity(){
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(KEY_LOGIN, variableBinding.editEmail.getText().toString());
+        editor.apply();
+        Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
+        nextPage.putExtra("EmailAddress", variableBinding.editEmail.getText().toString());
+        startActivity(nextPage);
     }
 }
--- app/src/main/java/algonquin/cst2335/rugg0011/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/rugg0011/SecondActivity.java
@@ -8,25 +8,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+
 
 import algonquin.cst2335.rugg0011.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
 
     ActivitySecondBinding variableBinding;
+    SharedPreferences prefs;
+    private static final String SHARED_PREF_NAME = "MyData";
+    private static final String KEY_PHONE = "phone";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +34,7 @@ public class SecondActivity extends AppCompatActivity {
 
         Intent fromPrevious = getIntent();
         String emailAddress = fromPrevious.getStringExtra("EmailAddress");
-        variableBinding.textView.setText("Welcome back\n" + emailAddress);
+        variableBinding.textView.setText("Welcome back " + emailAddress);
 
         variableBinding.callButton.setOnClickListener(clk -> {
             Intent call = new Intent(Intent.ACTION_DIAL);
@@ -45,6 +42,8 @@ public class SecondActivity extends AppCompatActivity {
             call.setData(Uri.parse("tel:" + phoneNumber));
             startActivity(call);
         });
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         ActivityResultLauncher<Intent>cameraResult = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -60,11 +59,32 @@ public class SecondActivity extends AppCompatActivity {
                 }
         );
 
-        variableBinding.picButton.setOnClickListener(clk -> {
-            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            cameraResult.launch(cameraIntent);
+        variableBinding.picButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                TakePicture(cameraResult);
+            }
         });
 
+        prefs = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
 
+        String phone = prefs.getString(KEY_PHONE, null);
+
+        variableBinding.editPhone.setText(phone);
+
+    }
+
+    void TakePicture(ActivityResultLauncher<Intent> cameraResult) {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraResult.launch(cameraIntent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        prefs = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(KEY_PHONE, variableBinding.editPhone.getText().toString());
+        editor.apply();
     }
 }
